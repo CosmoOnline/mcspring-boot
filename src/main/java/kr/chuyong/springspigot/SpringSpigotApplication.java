e70287fbbcc92ea8adb4255304971f7c5e7e0762
ChuYong <jakgon@naver.com>
2023-07-31 18:42:11 +0900

    feat: 스케쥴러 활성화

--- src/main/java/kr/chuyong/springspigot/SpringSpigotApplication.java
+++ src/main/java/kr/chuyong/springspigot/SpringSpigotApplication.java
@@ -3,9 +3,11 @@ package kr.chuyong.springspigot;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("kr.chuyong.springspigot")
 @EnableAutoConfiguration
+@EnableScheduling
 public class SpringSpigotApplication {
     public SpringSpigotApplication(DataSourceAutoConfiguration configuration) {
         System.out.println("DB : " + configuration);
