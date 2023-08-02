ac2412f44c4fd87a5d48da0262501060258f40d8
송영민 <jakgon@naver.com>
2023-08-02 23:24:30 +0900

    feat: 시리얼라이저 손보기

---
+++ src/main/kotlin/chuyong/springspigot/jpa/type/UlidType.kt
@@ -0,0 +1,63 @@
+package chuyong.springspigot.jpa.type
+
+import com.github.f4b6a3.ulid.Ulid
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+import org.hibernate.engine.spi.SharedSessionContractImplementor
+import org.hibernate.usertype.UserType
+import java.io.Serializable
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+class UlidType : UserType<Ulid> {
+    override fun equals(x: Ulid?, y: Ulid?): Boolean {
+        return x == y
+    }
+
+    override fun hashCode(x: Ulid?): Int {
+        return x.hashCode()
+    }
+
+    override fun getSqlType(): Int {
+        return java.sql.Types.VARCHAR
+    }
+
+    override fun returnedClass(): Class<Ulid> {
+        return Ulid::class.java
+    }
+
+    override fun nullSafeGet(
+        rs: ResultSet?,
+        position: Int,
+        session: SharedSessionContractImplementor?,
+        owner: Any?,
+    ): Ulid? {
+        return rs?.getString(position)?.let(Ulid::from)
+    }
+
+    override fun isMutable(): Boolean = false
+
+    override fun assemble(cached: Serializable?, owner: Any?): Ulid? {
+        return cached as Ulid?
+    }
+
+    override fun disassemble(value: Ulid?): Serializable? {
+        return value as Serializable?
+    }
+
+    override fun deepCopy(value: Ulid?): Ulid? {
+        return value
+    }
+
+    override fun nullSafeSet(
+        st: PreparedStatement?,
+        value: Ulid?,
+        index: Int,
+        session: SharedSessionContractImplementor?,
+    ) {
+        value?.apply {
+            st?.setString(index, toString())
+        }
+    }
+
+}
